f9218c75e9ad59d3524f98d7f8dbe330136905ee
Tamas Bela Feher <tfeher@nvidia.com>
2019-03-18 00:53:23 +0100

    Add larger test for SVC

--- cuML/test/svc_test.cu
+++ cuML/test/svc_test.cu
@@ -138,5 +138,82 @@ TEST(SvcSolverTest, RelabelTest) {
   CUDA_CHECK(cudaFree(y_relabeled_d));  
 }
 
+__global__ void init_training_vectors(float * x, int n_rows, int n_cols, float *y) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid < n_rows * n_cols) {
+      int i = tid % n_rows;
+      int k = tid / n_rows;
+      x [tid] = tid;
+      if (k==0) {
+          y[i] = (i%2)*2 - 1;
+      }
+    }
+    
+}
+
+TEST(SvcSolverTest, SvcTestLarge) {
+  int n_rows = 100;
+  int n_cols = 280;
+  int n_ws = n_rows;
+    
+  float *x_dev;
+  allocate(x_dev, n_rows*n_cols);
+  float *y_dev;
+  allocate(y_dev, n_rows);
+
+  int TPB = 256;
+  init_training_vectors<<<ceildiv(n_rows*n_cols,TPB), TPB>>>(x_dev, n_rows, n_cols, y_dev);
+  CUDA_CHECK(cudaPeekAtLastError());
+  
+  float epsilon = 0.001;
+  
+  float *dual_coefs;
+  int n_coefs;
+  int *idx;
+  float *x_support;
+  float b;
+  int *support_idx;
+  
+  svcFit(x_dev, n_rows, n_cols, y_dev, &dual_coefs, &n_coefs, &support_idx, &x_support, &b, 1.0f, 1e-3f);
+  
+  //ASSERT_LE(n_coefs, 4);
+  
+  float dual_coefs_host[4]; 
+  updateHost(dual_coefs_host, dual_coefs, n_coefs);  
+  
+  float ay = 0;
+  for (int i=0; i<n_coefs; i++) {
+    ay += dual_coefs_host[i];
+  }
+  // \sum \alpha_i y_i = 0
+  EXPECT_LT(abs(ay), 1.0e-6f);
+
+ 
+   float x_support_host[8];
+   updateHost(x_support_host, x_support, n_coefs * n_cols);
+
+   float w[] = {0,0};
+   
+   for (int i=0; i<n_coefs; i++) {
+       w[0] += x_support_host[i] * dual_coefs_host[i]; 
+       w[1] += x_support_host[i + n_coefs] * dual_coefs_host[i];      
+   }
+   // for linear separable problems (large C) it should be unique
+   // we should norm it and check the direction
+   //EXPECT_LT(abs(w[0] - (-0.4)), epsilon);
+   //EXPECT_LT(abs(w[1] - 1.2), epsilon);
+  
+   //EXPECT_FLOAT_EQ(b, -1.8f);
+ 
+
+  if (n_coefs > 0) {
+    CUDA_CHECK(cudaFree(dual_coefs));
+    CUDA_CHECK(cudaFree(support_idx));
+    CUDA_CHECK(cudaFree(x_support));
+  }
+  CUDA_CHECK(cudaFree(x_dev));
+  CUDA_CHECK(cudaFree(y_dev));
+}
+
 }; // end namespace SVM
 }; // end namespace ML
