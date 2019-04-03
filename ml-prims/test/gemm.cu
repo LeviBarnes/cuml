ad726e2cd072d332cc880f513d076a8734e93add
LeviBarnes <lbarnes@nvidia.com>
2019-04-03 09:44:09 -0700

    Test case for linalg/gemm w/ epilogue lambda
    Still fails, but needing to stash this for now

--- ml-prims/test/gemm.cu
+++ ml-prims/test/gemm.cu
@@ -16,6 +16,7 @@
 
 #include <gtest/gtest.h>
 #include "linalg/gemm.h"
+#include "linalg/cutlass_wrappers.h"
 
 
 namespace MLCommon {
@@ -34,6 +35,46 @@ void fill(T *arr, T val, int N) {
   const int nblks = ceildiv<int>(N, NTHREADS * NITEMS);
   fillKernel<T><<<nblks, NTHREADS>>>(arr, val, N);
   CUDA_CHECK(cudaPeekAtLastError());
+}
+
+class LambdaContainer {
+
+   public:
+   int (*null_init) ( cutlass::gemm::LinearScaling<float>::Params &) = 
+              [] __host__ __device__ (cutlass::gemm::LinearScaling<float>::Params &p) {return 0;};
+   float (*times_two) ( float, int) = 
+              [] __host__ __device__ (float a, int inx) { return 10.f;};
+
+   LambdaContainer() { }
+};
+
+#ifndef __CUDACC_EXTENDED_LAMBDA__
+#error "compile with --expt-extended-lamdba"
+#endif
+TEST(Gemm, Gemm_w_Epilogue) {
+  float *A, *B, *C, *D;
+  int M = 128, N = 128, K = 64;
+  CUDA_CHECK(cudaMalloc((void **)&A, sizeof(float) * M * K));
+  fill(A, 1.f, M * K);
+  CUDA_CHECK(cudaMalloc((void **)&B, sizeof(float) * K * N));
+  fill(B, 0.5f, K * N);
+  CUDA_CHECK(cudaMalloc((void **)&C, sizeof(float) * M * N));
+  fill(C, 2.f, M * N);
+  CUDA_CHECK(cudaMalloc((void **)&D, sizeof(float) * M * N));
+  CUDA_CHECK(cudaMemset(D, 0, sizeof(float) * M * N));
+  gemm<float, float, float, cutlass::Shape<8, 128, 128>>(
+    CUBLAS_OP_N, CUBLAS_OP_N, M, N, K, 0.5f, B, N, A, K, 0.5f, C, N, D,
+    LambdaContainer().null_init, LambdaContainer().times_two);
+  float *hD = new float[M * N];
+  updateHost<float>(hD, D, M * N);
+  for (int i = 0; i < M * N; ++i) {
+    ASSERT_FLOAT_EQ(0.5f * K + 2.f, hD[i]) << " @hD[" << i << "]";
+  }
+  delete[] hD;
+  CUDA_CHECK(cudaFree(A));
+  CUDA_CHECK(cudaFree(B));
+  CUDA_CHECK(cudaFree(C));
+  CUDA_CHECK(cudaFree(D));
 }
 
 TEST(Gemm, Gemm_128x128x8) {
